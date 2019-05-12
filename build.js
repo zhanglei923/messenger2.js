32abc5abfda3b983cd646703fc2a6ee81b4c72dc
zhanglei@xiaoshouyi.com <zhang.lei.923@gmail.com>
2019-05-13 00:34:30 +0800

    up

---
+++ build.js
@@ -0,0 +1,16 @@
+let webpack = require('webpack')
+let _ = require('lodash')
+let pathutil = require('path')
+//const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+let config = require('./webpack.config.dev')
+let compiler = webpack(config);
+compiler.run((err, stats) => {
+    console.log('err', err)
+});
+
+// config = require('./webpack.config.prod')
+// let compiler_p = webpack(config)
+// compiler_p.run((err, stats) => {
+//     console.log('err_p', err)
+// });
