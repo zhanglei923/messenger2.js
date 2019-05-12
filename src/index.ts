76620583ff404ecefe21440210b3d22023c588c0
zhanglei@xiaoshouyi.com <zhang.lei.923@gmail.com>
2019-05-13 03:50:42 +0800

    up

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,36 @@
 import listener from './listener';
 import requester from './requester';
-
-var a=1;
-
-
-if (window.addEventListener) {
-    console.log(1,window);
-    window.addEventListener("message", ()=>{
-        
-        console.log('222');
-    
-        interface Person {
-            firstName: string;
-            lastName: string;
-        }
-
-        function greeter(person: Person) {
-            return "Hello, " + person.firstName + " " + person.lastName;
-        }
-
-        let user = { firstName: "Jane", lastName: "User" };
-
-        document.body.innerHTML = greeter(user);
-    });
-} else if (window.attachEvent) {
-    console.log(2,window);
-    window.attachEvent("onmessage", ()=>{console.log(333);});
+if ((<any>window).addEventListener) {
+    (<any>window).addEventListener("onmessage", ()=>{})
+} else if ((<any>window).attachEvent) {
+    (<any>window).attachEvent("onmessage", ()=>{})
 }
+
+
+
+// var a=1;
+
+
+// if (window.addEventListener) {
+//     console.log(1,window);
+//     window.addEventListener("message", ()=>{
+        
+//         console.log('222');
+    
+//         interface Person {
+//             firstName: string;
+//             lastName: string;
+//         }
+
+//         function greeter(person: Person) {
+//             return "Hello, " + person.firstName + " " + person.lastName;
+//         }
+
+//         let user = { firstName: "Jane", lastName: "User" };
+
+//         document.body.innerHTML = greeter(user);
+//     });
+// } else if (window.attachEvent) {
+//     console.log(2,window);
+//     window.attachEvent("onmessage", ()=>{console.log(333);});
+// }
