f29e17b4150983c33da920e732f2c2fe4e8ca179
zhanglei@xiaoshouyi.com <zhang.lei.923@gmail.com>
2019-05-13 03:27:19 +0800

    up

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
+import listener from './listener';
+import requester from './requester';
+
+var a=1;
 
 
 if (window.addEventListener) {
     console.log(1,window);
-    window.addEventListener("message", ()=>{console.log('222');});
+    window.addEventListener("message", ()=>{
+        
+        console.log('222');
+    
+        interface Person {
+            firstName: string;
+            lastName: string;
+        }
+
+        function greeter(person: Person) {
+            return "Hello, " + person.firstName + " " + person.lastName;
+        }
+
+        let user = { firstName: "Jane", lastName: "User" };
+
+        document.body.innerHTML = greeter(user);
+    });
 } else if (window.attachEvent) {
     console.log(2,window);
     window.attachEvent("onmessage", ()=>{console.log(333);});
